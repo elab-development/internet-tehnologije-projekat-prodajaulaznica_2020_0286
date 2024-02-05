86f9f13969118b9ec62e5d5f36b920edc734383d
mm20200041 <mm20200041@student.fon.bg.ac.rs>
2024-02-06 00:01:06 +0100

    iskoriscen api za kreiranje random slika za prikaz slika dogadjaja

--- reactapp/src/components/DogadjajKartica.jsx
+++ reactapp/src/components/DogadjajKartica.jsx
@@ -1,13 +1,32 @@
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './DogadjajKartica.css';
 import image1 from './images/1.jpg';
-// {dogadjaj.slika}
+import axios from 'axios';
+ 
 const DogadjajKartica = ({ dogadjaj,onKupiClick}) => {
+    const [slikaUrl, setSlikaUrl] = useState('');
+
+    useEffect(() => {
+        // Funkcija koja dohvaća random sliku za dati tip događaja
+        const fetchSlika = async () => {
+            try {
+                 
+                const response = await axios.get(`https://api.unsplash.com/photos/random?query=${dogadjaj.tip.naziv}&client_id=mgvH-dYb-0DA4vZU8jJbPSh6dJr9p4BkoaBx9alFrKA`);
+                setSlikaUrl(response.data.urls.regular);
+            } catch (error) {
+                console.error('Greška pri dohvatanju slike sa Unsplash-a', error);
+              
+                setSlikaUrl(image1);  // Postavite neku podrazumevanu sliku u slučaju greške
+            }
+        };
+
+        fetchSlika();
+    }, [dogadjaj.tip.naziv]);
     return (
         <div className="dogadjaj-kartica">
             <div className="dogadjaj-slika-container">
-                <img src={image1} alt={dogadjaj.naziv} className="dogadjaj-slika" />
+             <img src={slikaUrl || image1} alt={dogadjaj.naziv} className="dogadjaj-slika" />
                 <span className="dogadjaj-tip">{dogadjaj.tip.naziv}</span>
             </div>
             <div className="dogadjaj-info">
